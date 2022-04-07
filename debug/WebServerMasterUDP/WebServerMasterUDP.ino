158d5cdea07c20f9c7a67beb0240e836b3263950
ljcmps01 <ljcmps01@gmail.com>
2022-04-07 11:25:54 -0300

    Funcion reloj NTP removida

--- debug/WebServerMasterUDP/WebServerMasterUDP.ino
+++ debug/WebServerMasterUDP/WebServerMasterUDP.ino
@@ -1,55 +1,23 @@
-/*
- * NO FUNCIONA LINEA 111 y 179 
- * pareciera que el error era por que el buffer de entrada es de tipo byte
- * cuando tendria que ser de tipo char
- * 
- * Probar con el char casteado al inicio de la declaracion
- * 
- * Probar cambiando el tipo de variable del buffer de entrada
-  Web Server
-
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
- 
+/*Se quitó la funcionalidad de reloj NTP
+ * postergada para futuras versiones el registro de tiempo
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-#define NTP_PACKET_SIZE 48
-
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 20, 200);
 
 
 //Set global variables for NTP requests
-unsigned int localPort = 8888;       // local port to listen for UDP packets
+unsigned int localPort = 26026;       // local port to listen for UDP packets
 
-const char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
-
-//const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-unsigned int horas;
-unsigned int minutos;
-unsigned int segundos;
+char incomingTemperature[UDP_TX_PACKET_MAX_SIZE];
 // A UDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
 
@@ -91,13 +59,11 @@ void setup() {
 
 
 void loop() {
-  sendNTPpacket(timeServer);
-  delay(100);
   int packetSize = Udp.parsePacket();
   if (packetSize) {
-    //Si el mensaje proviene del puerto 26026
-    //Se trata de una lectura de DHT de un cliente
-    if(Udp.remotePort()==26026){
+      //Apenas se recibe un paquete se guarda su entrada
+      Udp.read(incomingTemperature, packetSize);
+      
       Serial.print("Received packet of size ");
       Serial.println(packetSize);
       Serial.print("From ");
@@ -111,37 +77,15 @@ void loop() {
       Serial.print(", port ");
       Serial.println(Udp.remotePort());
   
-      // read the packet into packetBufffer
-      Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+      //Se imprime la temperatura leida
       Serial.println("Contents:");
-      Serial.println((char)packetBuffer);
-    }
-
-    //Caso contrario
-    //Es la respuesta del servidor NTP
-    //Con la hora UTC actual
-    else{
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);
-  
-      //MOVER A FUNCION?
-      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-      // combine the four bytes (two words) into a long integer
-      // this is NTP time (seconds since Jan 1 1900):
-      unsigned long secsSince1900 = highWord << 16 | lowWord;
-      const unsigned long seventyYears = 2208988800UL;
-      // subtract seventy years:
-      unsigned long epoch = secsSince1900 - seventyYears;
-     // get the hour, minute and second:
-      horas=(epoch  % 86400L) / 3600-3; // print the hour (86400 equals secs per day)
-      minutos=(epoch  % 3600) / 60; // print the minute (3600 equals secs per minute)
-      segundos=epoch % 60; // print the second
-    }
+      Serial.println(incomingTemperature); 
   }
-
-  // listen for incoming clients
+//   listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
+    
+    
     Serial.println("new client");
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
@@ -165,24 +109,8 @@ void loop() {
           client.println("<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">");
           client.println("</header>");
           client.println("<html>");
-          // output the value of each analog input pin
-          //for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-          //  int sensorReading = analogRead(analogChannel);
-          //  client.print("analog input ");
-          //  client.print(analogChannel);
-          //  client.print(" is ");
-          //  client.print(sensorReading);
-          //  client.println("<br />");
-          //}
-          while(horas==-1);
-          client.print(horas);
-          client.print(":");
-          client.print(minutos);
-          client.print(":");
-          client.print(segundos);
           client.print("| Sala |");
-          horas=-1;
-          client.print((char)packetBuffer);
+          client.print(incomingTemperature);
           client.print("°C ");
           client.print("<br />");
           client.print("</html>");
@@ -203,28 +131,4 @@ void loop() {
     client.stop();
     Serial.println("client disconnected");
   }
-}
-
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(const char * address) {
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); // NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
 }
